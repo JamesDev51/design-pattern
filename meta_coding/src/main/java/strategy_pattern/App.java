5d3b50bfbcb4d51452ea0d5b2461e01dcf1fb549
JamesWithCode <minseok.jeong.james@gmail.com>
2022-11-02 11:49:48 +0900

    221102 전략패턴

---
+++ meta_coding/src/main/java/strategy_pattern/App.java
@@ -0,0 +1,12 @@
+package strategy_pattern;
+
+public class App {
+	public static void main(String[] args) {
+		Mouse m = new Mouse();
+		Cat c = new Cat();
+		DoorMan dm = new DoorMan();
+
+		dm.쫒아내(m);
+		dm.쫒아내(c);
+	}
+}
---
+++ meta_coding/src/main/java/strategy_pattern/DoorMan.java
@@ -0,0 +1,11 @@
+package strategy_pattern;
+
+public class DoorMan {
+
+	// public void 쫒아내(Mouse m) {
+	// 	System.out.println(m.getName() + " 쫒아내");
+	// }
+	public void 쫒아내(Animal a) {
+		System.out.println(a.getName() + " 쫒아내");
+	}
+}
